723a8afccf5a9d81271e1b43c201cf1fec804823
Elton Fonseca <elton869@gmail.com>
2024-01-19 18:32:02 -0300

    11 - Criação de dados em massa

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    //protected $fillable = ['nome', 'cpf', 'data_contratacao', 'data_demissao'];
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 }
