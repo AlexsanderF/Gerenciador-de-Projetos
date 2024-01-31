9a66001e0d80ec655adea04dd0aa393b7a400d9a
Elton Fonseca <elton869@gmail.com>
2024-01-31 14:51:05 -0300

    19 - Mapeando os relacionamentos (1 para 1)

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
     use HasFactory;
+
+    /**
+     * Mapeia o relacionamento com funcionário
+     * Um endereço pertence a um funcionário
+     * 
+     * @return BelongsTo 
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
 {
@@ -12,4 +13,15 @@ class Employee extends Model
     //protected $fillable = ['nome', 'cpf', 'data_contratacao', 'data_demissao'];
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * Mapeia o relacionamento com o endereço
+     * Um funcionário tem um endereço
+     * 
+     * @return HasOne 
+     */
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
 }
