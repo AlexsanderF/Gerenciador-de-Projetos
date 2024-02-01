a0829fc9b7be7ac35945a90404b71999718a839c
Elton Fonseca <elton869@gmail.com>
2024-02-01 13:22:26 -0300

    24 - Mapeando os relacionamentos (1 para N)

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nome', 'endereco', 'descricao'];
+
+    /**
+     * Um cliente tem muitos projetos
+     * 
+     * @return HasMany
+     */
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory;
+
+    /**
+     * Um projeto pertence a um cliente
+     * 
+     * @return BelongsTo
+     */
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
 }
