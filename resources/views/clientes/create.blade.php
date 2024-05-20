5eb2e7dae17f627c94179a89a01e3905a3342ad8
Alexsander Fontes <sander-369@hotmail.com>
2024-05-20 16:56:50 -0300

    :sparkles: feat: Melhorando a usabilidade do usuário.

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -3,24 +3,27 @@
         <h1 class="text-4xl font-bold text-center my-4">
             Cadastrar novo Cliente
         </h1>
-        <form method="post" action="{{route('clientes.store')}}" class="max-w-6xl mx-auto">
+        <form method="post" action="{{ route('clientes.store') }}" class="max-w-6xl mx-auto">
             @csrf
             <div class="mb-5">
-                <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Nome do
-                    cliente</label>
-                <input type="text" id="nome" name="nome"
+                <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                    Nome do cliente
+                </label>
+                <input type="text" id="nome" name="nome" value="{{ old('nome') }}"
                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
             <div class="mb-5">
-                <label for="endereco" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Endereço do
-                    cliente</label>
-                <input type="text" id="endereco" name="endereco"
+                <label for="endereco" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                    Endereço do cliente
+                </label>
+                <input type="text" id="endereco" name="endereco" value="{{ old('endereco') }}"
                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
             <div class="mb-5">
-                <label for="descricao" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Descrição do
-                    cliente</label>
-                <input type="text" id="descricao" name="descricao"
+                <label for="descricao" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                    Descrição do cliente
+                </label>
+                <input type="text" id="descricao" name="descricao" value="{{ old('descricao') }}"
                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
             <button type="submit"
